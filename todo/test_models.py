5fab1fdf57e1748a306f9df58928eab38de90c6d
ddeveloper72 <duncan.niamh@gmail.com>
2018-11-15 21:57:53 +0000

    used Coverage to identify where additional testing was required and then wrote the tests.

--- todo/test_models.py
+++ todo/test_models.py
@@ -13,3 +13,7 @@ class TestItemsModel(TestCase):
         item.save()
         self.assertEqual(item.name, "Create a Test")
         self.assertTrue(item.done)
+
+    def test_item_as_a_string(self):
+        item = Item(name = "Create a Test")
+        self.assertEqual("Create a Test", str(item))
